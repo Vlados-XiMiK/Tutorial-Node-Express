2fc531f66429af00fac7726c4d1ce51479ea3a24
unknown <tvladislav0504@gmail.com>
2024-10-24 13:59:42 +0300

    65 Express all static

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const path = require('path')
+
 const app = express()
 
-app.get('', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
+app.use(express.static('./public'))
+
+// app.get('', (req, res) => {
+//  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// adding to static assets
+// SSR
+// })
 
 app.all('*', (req, res) => {
   res.status(404).send('resource nor found')
